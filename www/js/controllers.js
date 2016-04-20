1485d71c0647a3cdbb24110649caa58bec81b535
Waylon Brown <waylonjbrown@gmail.com>
2016-04-19 21:13:19 -0500

    Use local storage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,24 @@ angular.module('app.controllers', [])
 })
    
 .controller('settingsCtrl', function($scope) {
+	//input listeners
+	document.getElementById("message").addEventListener('input', function() {
+		if (localStorage != undefined) {
+			localStorage.setItem("message", document.getElementById("message").value)
+		}
+	});
 
+	if(localStorage != undefined)
+	{
+		if (localStorage.getItem("message") != undefined) {
+			document.getElementById("message").value = localStorage.getItem("message")
+		} else {
+			document.getElementById("message").value = "Your driver is on their way! View their drive here: www.exampleurl.com"
+		}
+	}
+	else
+	{
+	  console.log("No local storage support");
+	}
 })
     
