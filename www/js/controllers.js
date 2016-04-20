8859b653880c69a37cbcae3d362308268ac845ae
Waylon Brown <waylonjbrown@gmail.com>
2016-04-19 21:50:45 -0500

    Some progress

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,20 +5,47 @@ angular.module('app.controllers', [])
 })
    
 .controller('settingsCtrl', function($scope) {
+	var messageElement = document.getElementById("message")
+	var codeElement = document.getElementById("compcode")
+	var checkboxElement = document.getElementById("checkbox").getElementsByTagName('input')[0]
+
 	//input listeners
-	document.getElementById("message").addEventListener('input', function() {
+	messageElement.addEventListener('input', function() {
 		if (localStorage != undefined) {
-			localStorage.setItem("message", document.getElementById("message").value)
+			localStorage.setItem("message", messageElement.value)
+		}
+	});
+	codeElement.addEventListener('input', function() {
+		if (localStorage != undefined) {
+			localStorage.setItem("compcode", codeElement.value)
+		}
+	});
+	checkboxElement.addEventListener('click', function() {
+		console.log(checkboxElement.checked)
+		if (localStorage != undefined) {
+			localStorage.setItem("checkbox", checkboxElement.checked)
 		}
 	});
 
 	if(localStorage != undefined)
 	{
-		if (localStorage.getItem("message") != undefined) {
-			document.getElementById("message").value = localStorage.getItem("message")
-		} else {
-			document.getElementById("message").value = "Your driver is on their way! View their drive here: www.exampleurl.com"
+		console.log("Local storage supported.");
+
+		//set defaults
+		if (localStorage.getItem("message") == undefined) {
+			localStorage.setItem("message", "Your driver is on their way! View their drive here: www.exampleurl.com")
 		}
+		if (localStorage.getItem("checkbox") == undefined) {
+			localStorage.setItem("checkbox", false)
+		}
+		if (localStorage.getItem("compcode") == undefined) {
+			localStorage.setItem("compcode", "")
+		}
+
+		messageElement.value = localStorage.getItem("message")
+		codeElement.value = localStorage.getItem("compcode")
+		checkboxElement.checked = localStorage.getItem("checkbox")
+		console.log(localStorage.getItem("checkbox"))
 	}
 	else
 	{
