9dc0eb4f07dfea030d6dc1137e208ab8251befde
= <waylon.brown@mutualmobile.com>
2016-04-21 14:00:08 -0500

    Gets background location

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -35,6 +35,45 @@ angular.module('app.controllers', [])
 					}, function(error) {
 						window.plugins.toast.showShortBottom('Error sending text message')
 					});
+
+				////////
+				//GET BACKGROUND POSITION
+				////////
+				navigator.geolocation.getCurrentPosition();
+
+				var bgLocationServices = window.plugins.backgroundLocationServices;
+				bgLocationServices.configure({
+					 //Both
+				     desiredAccuracy: 1, // Desired Accuracy of the location updates (lower means more accurate but more battery consumption)
+				     distanceFilter: 5, // (Meters) How far you must move from the last point to trigger a location update
+				     debug: true, // <-- Enable to show visual indications when you receive a background location update
+				     interval: 10000, // (Milliseconds) Requested Interval in between location updates.
+				     //Android Only
+				     notificationTitle: 'Where They At', // customize the title of the notification
+				     notificationText: 'Sharing location, tap to stop sharing', //customize the text of the notification
+				     fastestInterval: 7000, // <-- (Milliseconds) Fastest interval your app / server can handle updates
+				     useActivityDetection: true // Uses Activitiy detection to shut off gps when you are still (Greatly enhances Battery Life)
+				});
+
+				//Register a callback for location updates, this is where location objects will be sent in the background
+				bgLocationServices.registerForLocationUpdates(function(location) {
+				     //console.log("We got a BG Update in registerForLocationUpdates" + JSON.stringify(location));
+				     console.log("We got a BG Update in registerForLocationUpdates" + location.latitude);
+				}, function(err) {
+				     console.log("Error: Didnt get an update", err);
+				});
+
+				//Register for Activity Updates (ANDROID ONLY)
+				//Uses the Detected Activies API to send back an array of activities and their confidence levels
+				//See here for more information: //https://developers.google.com/android/reference/com/google/android/gms/location/DetectedActivity
+				bgLocationServices.registerForActivityUpdates(function(activities) {
+				     console.log("We got a BG Update in registerForActivityUpdates" + activities);
+				}, function(err) {
+				     console.log("Error: Something went wrong", err);
+				});
+
+				//Start the Background Tracker. When you enter the background tracking will start, and stop when you enter the foreground.
+				bgLocationServices.start();
 			}
 		});
 		navigateButton.addEventListener('click', function() {
